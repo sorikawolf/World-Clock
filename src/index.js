b0ffeae944a63f536201fcc2f0459da0a6291d00
sorikawolf <jennsorikawolf@gmail.com>
2026-03-03 22:33:43 -0500

    Added current location to select

--- src/index.js
+++ src/index.js
@@ -30,6 +30,9 @@ setInterval(function () {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
