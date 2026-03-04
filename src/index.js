0050e8db8bb0f937069d3ebc032840edfe3a8f2c
sorikawolf <jennsorikawolf@gmail.com>
2026-03-03 21:45:24 -0500

    added functionality for the select

--- src/index.js
+++ src/index.js
@@ -27,3 +27,22 @@ setInterval(function () {
     .tz("America/Toronto")
     .format("h:mm:ss [<small>]A[</small>]");
 }, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+
+  citiesElement.innerHTML = ` <div class="city">
+      <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("h:mm:ss [<small>]A[</small>]")}</div>
+    </div>`;
+}
+
+let citiesSelectElement = document.querySelector("#selectedCity");
+
+citiesSelectElement.addEventListener("change", updateCity);
